fabc7c4f7a9eb512ff079deb68533c1590a66896
Marjose <hypernate1@gmail.com>
2023-07-19 14:13:33 +0800

    Initial commit :fire:

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')
+    ->prefix('v1')
+    ->group(function(){
+       /*
+        * Authentication Route
+        */
+        Route::post('/login',);
+    });
