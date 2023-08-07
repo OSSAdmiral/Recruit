c92c6fa05e8982065d3c074b4b574e8198190b1c
Marjose <hypernate1@gmail.com>
2023-08-07 12:57:37 +0800

    added section to group fields

--- app/Filament/Resources/JobOpeningsResource.php
+++ app/Filament/Resources/JobOpeningsResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\JobOpeningsResource\Pages;
 use App\Models\JobOpenings;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -24,53 +25,72 @@ class JobOpeningsResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('postingTitle')
-                    ->required(),
-                TextInput::make('NumberOfPosition')
-                    ->required(),
-                TextInput::make('JobTitle')
-                    ->required(),
-                TextInput::make('JobOpeningSystemID'),
-                TextInput::make('TargetDate')
-                    ->required(),
-                TextInput::make('Status')
-                    ->required(),
-                TextInput::make('Salary'),
-                TextInput::make('Department')
-                    ->required(),
-                TextInput::make('HiringManager'),
-                TextInput::make('AssignedRecruiters'),
-                TextInput::make('DateOpened')
-                    ->required(),
-                TextInput::make('JobType')
-                    ->required(),
-                TextInput::make('RequiredSkill')
-                    ->required(),
-                TextInput::make('WorkExperience')
-                    ->required(),
-                TextInput::make('JobDescription')
-                    ->required(),
-                TextInput::make('City')
-                    ->required(),
-                TextInput::make('Country')
-                    ->required(),
-                TextInput::make('State')
-                    ->required(),
-                TextInput::make('ZipCode')
-                    ->required(),
-                Checkbox::make('RemoteJob'),
-                TextInput::make('CreatedBy')
-                    ->required(),
-                TextInput::make('ModifiedBy')
-                    ->required(),
-                Placeholder::make('created_at')
-                    ->label('Created Date')
-                    ->content(fn (?JobOpenings $record): string => $record?->created_at?->diffForHumans() ?? '-'),
-
-                Placeholder::make('updated_at')
-                    ->label('Last Modified Date')
-                    ->content(fn (?JobOpenings $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
-            ])->columns(2);
+                Section::make('job-opening-information-section')
+                ->label('Job Opening Information')
+                ->icon('heroicon-o-briefcase')
+                ->schema([
+                    TextInput::make('postingTitle')
+                        ->required(),
+                    TextInput::make('NumberOfPosition')
+                        ->required(),
+                    TextInput::make('JobTitle')
+                        ->required(),
+                    TextInput::make('JobOpeningSystemID'),
+                    TextInput::make('TargetDate')
+                        ->required(),
+                    TextInput::make('Status')
+                        ->required(),
+                    TextInput::make('Salary'),
+                    TextInput::make('Department')
+                        ->required(),
+                    TextInput::make('HiringManager'),
+                    TextInput::make('AssignedRecruiters'),
+                    TextInput::make('DateOpened')
+                        ->required(),
+                    TextInput::make('JobType')
+                        ->required(),
+                    TextInput::make('RequiredSkill')
+                        ->required(),
+                    TextInput::make('WorkExperience')
+                        ->required(),
+                    Checkbox::make('RemoteJob'),
+                ])->columns(2),
+                Section::make('job-opening-address-information-section')
+                ->id('job-opening-address-information-section')
+                ->label('Address Information')
+                ->schema([
+                    TextInput::make('City')
+                        ->required(),
+                    TextInput::make('Country')
+                        ->required(),
+                    TextInput::make('State')
+                        ->required(),
+                    TextInput::make('ZipCode')
+                        ->required(),
+                ])->columns(2),
+                Section::make('job-opening-description-information')
+                ->id('job-opening-description-information')
+                ->label('Description Information')
+                ->schema([
+                    TextInput::make('JobDescription')
+                        ->required(),
+                ])->columns(1),
+                Section::make('job-opening-system-info')
+                ->id('job-opening-system-info')
+                ->label('System Information')
+                ->schema([
+                    TextInput::make('CreatedBy')
+                        ->required(),
+                    TextInput::make('ModifiedBy')
+                        ->required(),
+                    Placeholder::make('created_at')
+                        ->label('Created Date')
+                        ->content(fn (?JobOpenings $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+                    Placeholder::make('updated_at')
+                        ->label('Last Modified Date')
+                        ->content(fn (?JobOpenings $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+                ])->columns(2)
+            ]);
     }
 
     public static function table(Table $table): Table
