31697adecc1c78e4b2243dcf14c505ec555140d4
MarJose123 <MarJose123@users.noreply.github.com>
2023-08-13 04:32:37 +0000

    Fix styling :shirt:

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements  FilamentUser
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
