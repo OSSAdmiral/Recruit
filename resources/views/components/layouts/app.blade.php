2883f359f19ac0a60dd0df74be7e290f9f72ea15
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-08-31 17:37:57 +0800

    WIP

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,10 +13,10 @@
             display: none !important;
         }
     </style>
-    <link href="https://preview.cruip.com/job-board/style.css" rel="stylesheet">
 
     @filamentStyles
     @vite('resources/css/app.css')
+    @vite('resources/css/career.css')
 </head>
 
 <body class="antialiased">
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/career.css',
+                'resources/js/app.js'],
             refresh: [
                 'app/livewire/**'
             ],
