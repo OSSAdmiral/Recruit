f4775969f1b532e6010a560894940471289b2cf5
MarJose123 <MarJose123@users.noreply.github.com>
2023-09-01 03:18:15 +0000

    Fix styling :shirt:

--- app/Livewire/CareerLandingPage.php
+++ app/Livewire/CareerLandingPage.php
@@ -10,13 +10,16 @@ use Livewire\Component;
 class CareerLandingPage extends Component
 {
     public ?bool $showRemote;
+
     public ?array $jobTypeFilter;
+
     protected static array|Builder|null $jobTypeList;
 
     private static function queryTable(): Builder
     {
         return JobOpenings::query()->where('published_career_site', '=', true);
     }
+
     private static function jobTypes(): array|\Illuminate\Database\Eloquent\Collection
     {
         return self::$jobTypeList = self::queryTable()->select(['JobType'])->distinct()->get();
