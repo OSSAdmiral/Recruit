88fbcc30efd21fa956a019cc00814ae7e9f687c5
MarJose123 <MarJose123@users.noreply.github.com>
2023-09-01 03:59:25 +0000

    Fix styling :shirt:

--- app/Livewire/CareerLandingPage.php
+++ app/Livewire/CareerLandingPage.php
@@ -14,6 +14,7 @@ class CareerLandingPage extends Component
     public ?array $jobTypeFilter = [];
 
     public array|Builder|null $jobTypeList = [];
+
     public array|Builder|null $jobsList = [];
 
     public function mount()
@@ -28,14 +29,14 @@ class CareerLandingPage extends Component
 
     private function jobTypes(): array|\Illuminate\Database\Eloquent\Collection
     {
-       return $this->jobTypeList = static::queryTable()->count() > 0 ? [] : self::queryTable()->select(['JobType'])->distinct()->get()->toArray();
+        return $this->jobTypeList = static::queryTable()->count() > 0 ? [] : self::queryTable()->select(['JobType'])->distinct()->get()->toArray();
     }
 
     #[Title('Work with us')]
     public function render(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('livewire.career-landing-page', [
-            'jobList' => $this->jobsList
+            'jobList' => $this->jobsList,
         ]);
     }
 }
