3e70eaff2368795e6ed765f888a0bbcc471af317
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-09-01 11:17:17 +0800

    WIP

--- app/Livewire/CareerLandingPage.php
+++ app/Livewire/CareerLandingPage.php
@@ -11,15 +11,15 @@ class CareerLandingPage extends Component
 {
     public ?bool $showRemote;
     public ?array $jobTypeFilter;
-    protected static $jobTypeList;
+    protected static array|Builder|null $jobTypeList;
 
     private static function queryTable(): Builder
     {
         return JobOpenings::query()->where('published_career_site', '=', true);
     }
-    private static function jobTypes(): Builder
+    private static function jobTypes(): array|\Illuminate\Database\Eloquent\Collection
     {
-        return self::$jobTypeList = self::queryTable()->select(['JobType'])->distinct();
+        return self::$jobTypeList = self::queryTable()->select(['JobType'])->distinct()->get();
     }
 
     #[Title('Work with us')]
