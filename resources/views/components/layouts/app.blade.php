26f6e3b1f838bea4cd5dc919899b801ebb291157
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-09-13 10:01:34 +0800

    added csrf token

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ $title ? $title." | ".config('app.name') : config('app.name') }}</title>
