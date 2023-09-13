c2ea33a8a9eabbd3827bd4b1af695cf658c11e71
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-09-13 10:15:28 +0800

    remove button type

--- resources/views/career-form/apply-job-components/SubmitApplicationButton.blade.php
+++ resources/views/career-form/apply-job-components/SubmitApplicationButton.blade.php
@@ -1,5 +1,5 @@
 <div class="cglzl c0spu c7764">
-    <button class="ch5p0 comj7 c3fma cfkyn cv0zi cq3a6 csg05" type="submit">
+    <button class="ch5p0 comj7 c3fma cfkyn cv0zi cq3a6 csg05">
         Submit Application
     </button>
 </div>
