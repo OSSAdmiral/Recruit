f20f9e167e5bcc4f8e3df4b5a5431f1d4bddfb9a
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-09-14 17:37:13 +0800

    added the data for the job details

--- resources/views/livewire/career-job-detail.blade.php
+++ resources/views/livewire/career-job-detail.blade.php
@@ -52,21 +52,25 @@
                                                     <svg class="ccrmq cqfuo caano" width="14" height="14" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M9.707 4.293a1 1 0 0 0-1.414 1.414L10.586 8H2V2h3a1 1 0 1 0 0-2H2a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h8.586l-2.293 2.293a1 1 0 1 0 1.414 1.414l4-4a1 1 0 0 0 0-1.414l-4-4Z"></path>
                                                     </svg>
-                                                    <span class="cza13 ckc7d">24 August, 2024</span>
+                                                    <span class="cza13 ckc7d">
+                                                        {{\Carbon\Carbon::createFromTimeStamp(strtotime($jobDetails->created_at))->calendar()}}
+                                                    </span>
                                                 </li>
                                                 <li class="cqho4 csqne">
                                                     <svg class="ccrmq cqfuo caano" width="14" height="16" xmlns="http://www.w3.org/2000/svg">
                                                         <circle cx="7" cy="7" r="2"></circle>
                                                         <path d="M6.3 15.7c-.1-.1-4.2-3.7-4.2-3.8C.7 10.7 0 8.9 0 7c0-3.9 3.1-7 7-7s7 3.1 7 7c0 1.9-.7 3.7-2.1 5-.1.1-4.1 3.7-4.2 3.8-.4.3-1 .3-1.4-.1Zm-2.7-5 3.4 3 3.4-3c1-1 1.6-2.2 1.6-3.6 0-2.8-2.2-5-5-5S2 4.2 2 7c0 1.4.6 2.7 1.6 3.7 0-.1 0-.1 0 0Z"></path>
                                                     </svg>
-                                                    <span class="cza13 ckc7d">London, UK / Remote friendly</span>
+                                                    <span class="cza13 ckc7d">
+                                                        {{$jobDetails->RemoteJob === 1 ? 'Fully Remote' : 'On-site'}}
+                                                    </span>
                                                 </li>
                                                 <li class="cqho4 csqne">
                                                     <svg class="ccrmq cqfuo caano" width="16" height="12" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M15 0H1C.4 0 0 .4 0 1v10c0 .6.4 1 1 1h14c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm-1 10H2V2h12v8Z"></path>
                                                         <circle cx="8" cy="6" r="2"></circle>
                                                     </svg>
-                                                    <span class="cza13 ckc7d">$75K - $100K</span>
+                                                    <span class="cza13 ckc7d">{{$jobDetails->Salary}}</span>
                                                 </li>
                                             </ul>
                                         </div>
@@ -135,47 +139,33 @@
                                     <div class="cnog5">
                                         <a class="cvqf0 crqt4" href="{{route('career.landing_page')}}"><span class="c8b8n">&lt;-</span> All Jobs</a>
                                     </div>
-                                    <h1 class="c5zpx c9gkl c8hbn cn95v">Engineering Manager Developer Experience</h1>
+                                    <h1 class="c5zpx c9gkl c8hbn cn95v">{{$jobDetails->postingTitle}}</h1>
                                     <!-- Job description -->
                                     <div class="c5rk9 coxki">
-                                        <div>
+                                        {{--<div>
                                             <h3 class="c8tys c89yv chhg4 codx4">About Us</h3>
                                             <div class="cdfls cttum">
                                                 <p>In the world of AI, behavioural predictions are leading the charge to better machine learning.</p>
                                                 <p>There is so much happening in the AI space. Advances in the economic sectors have seen automated business practices rapidly increasing economic value. While the realm of the human sciences has used the power afforded by computational capabilities to solve many human based dilemmas. Even the art scene has adopted carefully selected ML applications to usher in the technological movement.</p>
                                                 <p>As a Senior Client Engineer, you'll work alongside other engineers, designers, and product managers to tackle everything from huge company initiatives to modest but important bug fixes, from start to finish. You'll also collaborate with your product team on discovery, helping to assess the direction and feasibility of product changes. And, perhaps most importantly, you'll actively contribute to the evolution of the culture and processes of a growing engineering team.</p>
                                             </div>
-                                        </div>
+                                        </div>--}}
                                         <div>
                                             <h3 class="c8tys c89yv chhg4 codx4">Job Description</h3>
                                             <div class="cdfls cttum">
-                                                <p>You love building great software. Your work could be supporting new feature development, migrating existing features, and creating other mobile and web solutions for customers. You'll have a primary focus on frontend development using Javascript. Our client's tech stack is JavaScript, primarily using React. A strong understanding of JS core (ES2019+) is required, with some exposure in Java as back-end technology. We use modern tools, which means you'll have the opportunity to work with Webpack, Redux, Apollo, Styled Components, and much more.</p>
-                                                <p>You love learning. Engineering is an ever-evolving world. You enjoy playing with new tech and exploring areas that you might not have experience with yet. You are self-driven, self-learner willing to share knowledge and participate actively in your community.</p>
-                                                <p>Having overlap with your team is critical when working in a global remote team. Modus requires all team members to overlap with EST morning hours daily. In addition, reliable high speed internet is a must.</p>
+                                                {!! $jobDetails->JobDescription !!}
                                             </div>
                                         </div>
                                         <div>
                                             <h3 class="c8tys c89yv chhg4 codx4">Requirements</h3>
-                                            <div class="cdfls cttum">
-                                                <p>We are a fast-growing, and remote-first company, so you'll likely get experience on many different projects across the organization. That said, here are some things you'll probably do:</p>
-                                                <ul class="cbjmj ckxyp cttum">
-                                                    <li>Give back to the community via open source and blog posts</li>
-                                                    <li>Travel and meet great people- as part of our remote-first lifestyle, it's important that we come together as needed to work together, meet each other in person and have fun together. Please keep that in mind when you apply</li>
-                                                    <li>Teach and be taught: Modus creates active teams that work in internal and external projects together, giving opportunities to stay relevant with the latest technologies and learning from experts worldwide</li>
-                                                    <li>Interact directly with internal and external clients to represent Modus and its values</li>
-                                                </ul>
+                                            <div class="cdfls cttum cbjmj ckxyp ">
+                                                {!! $jobDetails->JobRequirement !!}
                                             </div>
                                         </div>
                                         <div>
                                             <h3 class="c8tys c89yv chhg4 codx4">Benefits</h3>
-                                            <div class="cdfls cttum">
-                                                <p>We are a fast-growing, and remote-first company, so you'll likely get experience on many different projects across the organization. That said, here are some things you'll probably do:</p>
-                                                <ul class="cbjmj ckxyp cttum">
-                                                    <li>Give back to the community via open source and blog posts</li>
-                                                    <li>Travel and meet great people- as part of our remote-first lifestyle, it's important that we come together as needed to work together, meet each other in person and have fun together. Please keep that in mind when you apply</li>
-                                                    <li>Teach and be taught: Modus creates active teams that work in internal and external projects together, giving opportunities to stay relevant with the latest technologies and learning from experts worldwide</li>
-                                                    <li>Interact directly with internal and external clients to represent Modus and its values</li>
-                                                </ul>
+                                            <div class="cdfls cttum cbjmj ckxyp">
+                                                {!! $jobDetails->JobBenefits !!}
                                             </div>
                                         </div>
                                     </div>
