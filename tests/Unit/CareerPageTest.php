489cd81c66008ec8ae49c4e84b255655733f9e1e
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-09-14 13:32:05 +0800

    added test for career landing page

--- tests/Unit/CareerPageTest.php
+++ tests/Unit/CareerPageTest.php
@@ -1,7 +1,12 @@
 <?php
-
+namespace Tests\Unit;
+use App\Livewire\CareerLandingPage;
 use function Pest\Livewire\livewire;
+use function Pest\Laravel\{get};
 
-it('can see the career page', function () {
+it('can load the career page', function () {
     livewire(\App\Livewire\CareerLandingPage::class)->assertStatus(200);
+});
+it('can see the component for the landing page', function () {
+    get('/career')->assertSeeLivewire(CareerLandingPage::class);
 });
