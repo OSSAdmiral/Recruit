7be9aa61562d58d6bd6faf880ff41631a11f2d74
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-17 13:14:56 +0800

    added delete and edit action
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Filament/Resources/JobOpeningsResource/Pages/ViewJobOpenings.php
+++ app/Filament/Resources/JobOpeningsResource/Pages/ViewJobOpenings.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\JobOpeningsResource\Pages;
 
 use App\Filament\Resources\JobOpeningsResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewJobOpenings extends ViewRecord
 {
     protected static string $resource = JobOpeningsResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            DeleteAction::make(),
+            EditAction::make(),
+        ];
+    }
 }
