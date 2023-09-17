cab495d7959d9ca12729493d79c8d33cda8cc667
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-17 12:53:20 +0800

    change navigation sort number
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Filament/Resources/JobCandidatesResource.php
+++ app/Filament/Resources/JobCandidatesResource.php
@@ -24,7 +24,7 @@ class JobCandidatesResource extends Resource
 
     protected static ?string $recordTitleAttribute = 'postingTitle';
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 2;
 
     public static function form(Form $form): Form
     {
