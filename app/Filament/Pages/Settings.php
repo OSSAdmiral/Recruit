86be7e0f87a7ea3dd7f760e08289c5443c13d16e
MarJose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-18 17:58:31 +0800

    WIP

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -6,6 +6,7 @@ use Filament\Pages\Page;
 
 class Settings extends Page
 {
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.settings-modals';
@@ -16,4 +17,7 @@ class Settings extends Page
     {
 
     }
+
+
+
 }
--- resources/views/filament/pages/settings-modals.blade.php
+++ resources/views/filament/pages/settings-modals.blade.php
@@ -1,3 +1,10 @@
 <x-filament-panels::page>
-
+    <x-filament::tabs label="Content tabs" contained="true">
+        <x-filament::tabs.item>
+            Company Details
+        </x-filament::tabs.item>
+        <x-filament::tabs.item>Users
+        </x-filament::tabs.item>
+        <x-filament::tabs.item>Security Control</x-filament::tabs.item>
+    </x-filament::tabs>
 </x-filament-panels::page>
