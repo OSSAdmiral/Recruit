9b6a7bede8d8a4f729ac3a6cf1adbb66615736f5
MarJose123 <MarJose123@users.noreply.github.com>
2023-09-18 09:59:08 +0000

    Fix styling :shirt:

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -6,7 +6,6 @@ use Filament\Pages\Page;
 
 class Settings extends Page
 {
-
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.settings-modals';
@@ -17,7 +16,4 @@ class Settings extends Page
     {
 
     }
-
-
-
 }
