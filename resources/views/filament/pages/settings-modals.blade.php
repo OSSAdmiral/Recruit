12d884ce5807a301b442f84808d5c874937c81f1
Marjose <18107626+MarJose123@users.noreply.github.com>
2023-09-19 12:59:12 +0800

    WIP
    Signed-off-by: Marjose <18107626+MarJose123@users.noreply.github.com>

--- resources/views/filament/pages/settings-modals.blade.php
+++ resources/views/filament/pages/settings-modals.blade.php
@@ -1,9 +1,11 @@
 <x-filament-panels::page>
-    <x-filament::tabs label="Content tabs" contained="true">
-        <x-filament::tabs.item>
+    <x-filament::tabs label="Content tabs" contained="true" x-data="{ activeTab: 'tab1' }">
+        <x-filament::tabs.item alpine-active="activeTab === 'tab1'"
+                               x-on:click="activeTab = 'tab1'">
             Company Details
         </x-filament::tabs.item>
-        <x-filament::tabs.item>Users
+        <x-filament::tabs.item alpine-active="activeTab === 'tab2'"
+                               x-on:click="activeTab = 'tab2'">Users
         </x-filament::tabs.item>
         <x-filament::tabs.item>Security Control</x-filament::tabs.item>
     </x-filament::tabs>
