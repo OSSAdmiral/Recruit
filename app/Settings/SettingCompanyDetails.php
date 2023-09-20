357aa715d560299dd84e4a6322e872b6bd650faf
Marjose <18107626+MarJose123@users.noreply.github.com>
2023-09-20 13:10:21 +0800

    added spatie settings

---
+++ app/Settings/SettingCompanyDetails.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Settings;
+
+use Spatie\LaravelSettings\Settings;
+
+class SettingCompanyDetails extends Settings
+{
+    public ?string $site_name;
+    public ?string $company_name;
+    public ?string $company_website;
+    public ?string $company_primary_contact_email;
+    public ?string $company_profile;
+    public ?int $company_employee_count;
+    public ?string $company_country;
+    public ?string $company_state;
+    public ?string $company_city;
+
+
+    public static function group(): string
+    {
+        return 'company';
+    }
+}
---
+++ database/settings/2023_09_20_045657_create_company_detail_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('company.site_name', 'RecruitLab');
+        $this->migrator->add('company.company_name', 'RecruitLab');
+        $this->migrator->add('company.company_website', 'www.recruitlab.com');
+        $this->migrator->add('company.company_primary_contact_email', 'help@recruitlab.com');
+        $this->migrator->add('company.company_profile', 'An Opensource Software that focus on providing SMB free Recruitment system.');
+        $this->migrator->add('company.company_employee_count', 2);
+        $this->migrator->add('company.company_country', 'Philippines');
+        $this->migrator->add('company.company_state', 'Leyte');
+        $this->migrator->add('company.company_city', 'Tacloban');
+
+    }
+};
