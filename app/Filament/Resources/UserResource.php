569eca5e1014817d456ba596bf49ca0a4f115a9a
Marjose <18107626+MarJose123@users.noreply.github.com>
2023-09-21 12:08:51 +0800

    WIP

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?int $navigationSort = 1;
+
+    protected static ?string $navigationGroup = 'Security & Control';
+
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+               Tables\Columns\ImageColumn::make('profile_photo_path')
+                   ->label('Profile Photo')
+                   ->defaultImageUrl(url(filled($avatar = auth()->user()->getFilamentAvatarUrl()) ? $avatar : filament()->getUserAvatarUrl(auth()->user())))
+                   ->circular(),
+               Tables\Columns\TextColumn::make('name'),
+               Tables\Columns\TextColumn::make('email'),
+               Tables\Columns\BooleanColumn::make('email_verified_at'),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+//            ->where('id', '!=', auth()->user()->id)
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
