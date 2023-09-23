780e84a5a327a6abc03579b1426b55cfec295226
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-23 22:04:45 +0800

    added publish/unpublished action
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Filament/Resources/JobOpeningsResource/Pages/ViewJobOpenings.php
+++ app/Filament/Resources/JobOpeningsResource/Pages/ViewJobOpenings.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\JobOpeningsResource\Pages;
 
 use App\Filament\Resources\JobOpeningsResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewJobOpenings extends ViewRecord
 {
@@ -14,8 +17,39 @@ class ViewJobOpenings extends ViewRecord
     protected function getActions(): array
     {
         return [
-            DeleteAction::make(),
-            EditAction::make(),
+            Action::make('publish')
+                ->color('warning')
+                ->icon('heroicon-o-arrow-uturn-up')
+                ->tooltip('Publish this opening job to the career page')
+                ->label('Publish')
+                ->hidden(fn(Model $record) => $record->published_career_site === 1 ?? false)
+                ->action(function (Model $record){
+                    $record->published_career_site = 1;
+                    $record->save();
+                    Notification::make()
+                        ->body('Job Opening has been published.')
+                        ->success()
+                        ->send();
+                }),
+            Action::make('unpublished')
+                ->color('warning')
+                ->icon('heroicon-o-arrow-uturn-left')
+                ->tooltip('Unpublished this opening job in the career page')
+                ->label('Unpublished')
+                ->requiresConfirmation()
+                ->hidden(fn(Model $record) => $record->published_career_site === 0 ?? false)
+                ->action(function (Model $record){
+                    $record->published_career_site = 0;
+                    $record->save();
+                    Notification::make()
+                        ->body('Job Opening has been unpublished.')
+                        ->success()
+                        ->send();
+                }),
+            EditAction::make()
+                ->icon('heroicon-o-pencil-square'),
+            DeleteAction::make()
+                ->icon('heroicon-o-trash'),
         ];
     }
 }
