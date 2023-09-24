bdabc5934ea33b644393186e69ad83cf701e6389
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-24 20:40:46 +0800

    removed user extend
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Models/CandidateUser.php
+++ app/Models/CandidateUser.php
@@ -4,15 +4,33 @@ namespace App\Models;
 
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
 
-class CandidateUser extends Authenticatable implements FilamentUser
+class CandidateUser extends Model implements
+    FilamentUser,
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
-    use AuthenticationLoggable, HasApiTokens, HasFactory, Notifiable;
+    use AuthenticationLoggable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use Authenticatable;
+    use Authorizable;
+    use CanResetPassword;
+    use MustVerifyEmail;
 
     protected $fillable = [
         'name',
