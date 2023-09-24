fd892e9300233658419833e926fad29e7cef7fbd
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-24 20:42:00 +0800

    WIP
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\CandidateUser;
 use App\Models\User;
+use Illuminate\Auth\SessionGuard;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,8 +27,20 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        /* Bypass Policy for Candidate Portal User */
+        if(Auth::guard('candidate_web')->check())
+        {
+            Gate::before(function (?CandidateUser $user){
+                ddd($user);
+            });
+        }
+
+
+
        /* Gate::before(function (User $user, string $ability) {
             return $user->isSuperAdmin() ? true : null;
         });*/
+
+
     }
 }
