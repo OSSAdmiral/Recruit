f2c58fdf8234220245c4e7e58feb0ddc5894e1b1
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-09-29 20:05:29 +0800

    added save model
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

---
+++ database/migrations/2023_09_29_195504_create_saved_jobs_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('saved_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('jobopening');
+            $table->foreignId('record_owner');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('saved_jobs');
+    }
+};
