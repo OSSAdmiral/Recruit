a0c7210dee9b20969eb5b106e02e8c19b1f96d3e
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-11-01 15:29:07 +0800

    removed edit action
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

---
+++ app/Filament/Candidate/Resources/AppliedJobListResource/Pages/ListAppliedJobLists.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Candidate\Resources\AppliedJobListResource\Pages;
+
+use App\Filament\Candidate\Resources\AppliedJobListResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAppliedJobLists extends ListRecords
+{
+    protected static string $resource = AppliedJobListResource::class;
+}
