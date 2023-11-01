703133c74fbb62678e7a57af4ea3b648115e4f37
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-11-01 15:33:22 +0800

    WIP
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

---
+++ app/Filament/Candidate/Resources/AppliedJobListResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Candidate\Resources;
+
+use App\Filament\Candidate\Resources\AppliedJobListResource\Pages;
+use App\Filament\Candidate\Resources\AppliedJobListResource\RelationManagers;
+use App\Models\JobCandidates;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class AppliedJobListResource extends Resource
+{
+    protected static ?string $model = JobCandidates::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel = "My Applied Jobs";
+
+    protected static ?string $modelLabel = "Applied Job";
+
+    protected static ?string $pluralModelLabel = "Applied Jobs";
+
+    protected static ?int $navigationSort = 2;
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::where('Email', '=', auth()->user()->email)->count() > 0 ?? '';
+    }
+
+    public static function getNavigationBadgeColor(): ?string
+    {
+        return static::getModel()::where('Email', '=', auth()->user()->email)->count() > 0 ? 'success' : '';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->striped()
+            ->columns([
+                Tables\Columns\TextColumn::make('JobCandidateId')
+                    ->label('Job Candidate Number'),
+                Tables\Columns\TextColumn::make('job.postingTitle')
+                    ->label('Job Title'),
+                Tables\Columns\BooleanColumn::make('job.RemoteJob')
+                    ->label('Remote Job'),
+                Tables\Columns\TextColumn::make('CandidateStatus')
+                    ->label('Application Status'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+            ])
+            ->emptyStateHeading('No Job Applied.')
+            ->emptyStateDescription('Once you applied a job, it will appear here.')
+            ->emptyStateIcon('heroicon-o-briefcase')
+            ;
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->where('Email', '=', auth()->user()->email);
+    }
+
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAppliedJobLists::route('/'),
+        ];
+    }
+}
