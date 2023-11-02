d5c51771ed2de7437109c672370a9f1d096293f2
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-11-02 16:56:04 +0800

    added notifiable
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Models/Candidates.php
+++ app/Models/Candidates.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Candidates extends Model
 {
-    use AutoNumberTrait, HasFactory, SoftDeletes;
+    use AutoNumberTrait, HasFactory, SoftDeletes, Notifiable;
 
     protected $fillable = [
         'CandidateId',
