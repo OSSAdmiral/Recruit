8df957c73d6d8d2d1e6a879850cf463a5260cde8
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-11-04 12:59:27 +0800

    added simple page version without auth checking
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

---
+++ resources/views/components/layouts/simple.blade.php
@@ -0,0 +1,21 @@
+<x-filament-panels::layout.base :livewire="$livewire">
+    @props([
+        'after' => null,
+        'heading' => null,
+        'subheading' => null,
+    ])
+
+    <div class="fi-simple-layout flex min-h-screen flex-col items-center">
+        <div
+            class="fi-simple-main-ctn flex w-full flex-grow items-center justify-center"
+        >
+            <main
+                class="fi-simple-main my-16 w-full bg-white px-6 py-12 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 sm:max-w-lg sm:rounded-xl sm:px-12"
+            >
+                {{ $slot }}
+            </main>
+        </div>
+
+        {{ \Filament\Support\Facades\FilamentView::renderHook('panels::footer') }}
+    </div>
+</x-filament-panels::layout.base>
