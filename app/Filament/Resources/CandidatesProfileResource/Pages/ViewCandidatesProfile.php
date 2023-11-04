b8ea7d3e98a462f6a41c75af8cee1cb894a27a79
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-11-04 11:39:08 +0800

    added validation if the candidate is already existing or not
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Filament/Resources/CandidatesProfileResource/Pages/ViewCandidatesProfile.php
+++ app/Filament/Resources/CandidatesProfileResource/Pages/ViewCandidatesProfile.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\CandidatesProfileResource\Pages;
 use App\Filament\Resources\CandidatesProfileResource;
 use App\Models\candidatePortalInvitation;
 use App\Models\Candidates;
+use App\Models\CandidateUser;
 use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Notifications\Notification;
@@ -21,18 +22,32 @@ class ViewCandidatesProfile extends ViewRecord
     {
         return [
             Actions\EditAction::make()
-                ->icon('heroicon-o-pencil-square'),
-            Actions\Action::make('portal_invite')
-                ->icon('heroicon-o-envelope')
-                ->color(Color::Green)
-                ->label('Invite to Portal')
-                ->action(fn () => $this->portalInvite()),
+                ->icon('heroicon-m-pencil-square'),
+            Actions\ActionGroup::make([
+                Actions\Action::make('portal_invite')
+                    ->icon('heroicon-o-envelope')
+                    ->color(Color::Green)
+                    ->label('Invite to Portal')
+                    ->close()
+                    ->action(fn () => $this->portalInvite()),
+            ])
         ];
     }
 
     protected function portalInvite(): void
     {
         $candidate = Candidates::find($this->record->id)->first();
+
+        if($this->candidateUserModel()->count() > 0)
+        {
+            Notification::make('invitation_error')
+                ->warning()
+                ->title('Candidate Already Registered')
+                ->body('The candidate you\'re inviting is already registered in the portal.')
+                ->send();
+            return;
+        }
+
         if ($this->isAlreadyInvited()) {
             $existing_invite = $this->portalInvitationModel()->first();
             $id = $existing_invite->id;
@@ -40,6 +55,7 @@ class ViewCandidatesProfile extends ViewRecord
             $candidate->notifyNow(new \App\Notifications\CandidatePortalInvitation($candidate, $invite_link));
             // update the date sent
             $existing_invite->touch('sent_at');
+
         } else {
             $invite = candidatePortalInvitation::create([
                 'name' => "{$this->record->FirstName} {$this->record->LastName}",
@@ -49,7 +65,13 @@ class ViewCandidatesProfile extends ViewRecord
             $invite_link = URL::signedRoute('portal.invite', ['id' => $invite->id]);
             $candidate->notifyNow(new \App\Notifications\CandidatePortalInvitation($candidate, $invite_link));
         }
+        $this->notificatioinInviteSent();
 
+
+    }
+
+    protected function notificatioinInviteSent(): void
+    {
         Notification::make('invitation_success')
             ->success()
             ->icon('heroicon-o-envelope')
@@ -61,6 +83,11 @@ class ViewCandidatesProfile extends ViewRecord
     protected function portalInvitationModel(): Builder
     {
         return candidatePortalInvitation::whereEmail($this->record->email);
+    }
+
+    protected function candidateUserModel(): Builder
+    {
+        return CandidateUser::whereEmail($this->record->email);
     }
 
     protected function isAlreadyInvited(): bool
