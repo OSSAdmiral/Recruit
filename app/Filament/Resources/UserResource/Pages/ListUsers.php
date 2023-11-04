e245655c035af75b0acac8c9a69f13c8848d036f
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2023-11-04 23:09:29 +0800

    change button label
    Signed-off-by: Marjose Darang <18107626+MarJose123@users.noreply.github.com>

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -13,7 +13,8 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Invite User'),
         ];
     }
 }
